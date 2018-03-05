ed303ba8b24eb0a169dbfc2adde77c7af67538b1
Prince1126090 <prince1126090@gmail.com>
2018-03-05 11:10:25 +0600

    initial commit

---
+++ Delete_metered_bill.sql
@@ -0,0 +1,7 @@
+Delete BILLING_READING_MAP Where Bill_Id like '201801011000070%';
+Delete BILL_METERED Where Bill_Id like '201801011000070%';             
+Delete DTL_MARGIN_GOVT Where Bill_Id like '201801011000070%';
+Delete DTL_MARGIN_PB Where Bill_Id like '201801011000070%';
+Delete SUMMARY_MARGIN_GOVT Where Bill_Id like '201801011000070%';
+Delete SUMMARY_MARGIN_PB Where Bill_Id like '201801011000070%';
+Delete SALES_REPORT Where Bill_Id like '201801011000070%';
