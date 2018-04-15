3108cdff6a0f635ae2e6cc459d973bb138167967
Prince1126090 <prince1126090@gmail.com>
2018-04-15 13:02:30 +0600

    april 15

---
+++ Duplicate_row_meter_reading.sql
@@ -0,0 +1,17 @@
+/* Formatted on 4/15/2018 12:45:33 PM (QP5 v5.287) */
+SELECT *
+  FROM METER_READING
+ WHERE     ROWID NOT IN
+              (  SELECT MIN (ROWID)
+                   FROM METER_READING
+                  WHERE     CUSTOMER_ID LIKE '2410%'
+                        AND BILLING_YEAR = 2018
+                        AND BILLING_MONTH = 3
+               GROUP BY CUSTOMER_ID,
+                        BILLING_YEAR,
+                        BILLING_MONTH,
+                        PREV_READING,
+                        CURR_READING)
+       AND CUSTOMER_ID LIKE '2410%'
+       AND BILLING_YEAR = 2018
+       AND BILLING_MONTH = 3
